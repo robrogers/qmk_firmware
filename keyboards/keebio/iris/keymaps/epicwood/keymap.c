c788692271373abcfa9cb76b4c46990738f10a4a
U-VALIDATEK-CHESS\rob.rogers-admin <VALIDATEK-CHESS+rob.rogers-admin@validatek-chess-6.CHESS.local>
2020-02-11 14:44:39 -0500

    swapped left ctrl and shift

--- keyboards/keebio/iris/keymaps/epicwood/keymap.c
+++ keyboards/keebio/iris/keymaps/epicwood/keymap.c
@@ -5,6 +5,8 @@
 #define _RAISE 2
 #define _ADJUST 3
 
+//Change
+
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
@@ -20,9 +22,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+     KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,           KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,           KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_LALT, LOWER,   KC_SPC,                    KC_ENT,  RAISE,   KC_RGUI
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
